47ec7e667c6404afabb688f83ff78e184a3b10aa
Lara van Diemen <lara@yard.nl>
2023-05-26 15:00:36 +0200

    (chore): add build assets collapse and collapse-item

--- build/Blocks/collapse/index.asset.php
+++ build/Blocks/collapse/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a8245dabd73351e3b74e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '282a746f9af740c9d639');
