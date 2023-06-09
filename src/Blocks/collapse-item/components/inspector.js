a5916259e7f9dcd8ab31356ebed99bfb27021780
Lara van Diemen <lara@yard.nl>
2023-06-09 17:18:13 +0200

    (feat): add icon option to collapse-item block

--- src/Blocks/collapse-item/components/inspector.js
+++ src/Blocks/collapse-item/components/inspector.js
@@ -2,12 +2,23 @@
  * WordPress dependencies
  */
 import { InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, ToggleControl } from '@wordpress/components';
+import { PanelBody, TextControl, ToggleControl } from '@wordpress/components';
+import { applyFilters } from '@wordpress/hooks';
 import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import IconPickerControl from '../../../EditorComponents/icon-picker-control';
 
 const Inspector = ( props ) => {
 	const { attributes, setAttributes } = props;
-	const { isOpen } = attributes;
+	const { icon, iconAltText, isOpen } = attributes;
+
+	const enableIcon = applyFilters(
+		'yard-gutenberg.enable-collapse-icon',
+		true
+	);
 
 	return (
 		<InspectorControls>
@@ -18,6 +29,32 @@ const Inspector = ( props ) => {
 					onChange={ ( value ) => setAttributes( { isOpen: value } ) }
 				/>
 			</PanelBody>
+			{ enableIcon && (
+				<PanelBody title={ __( 'Icoon instellingen' ) }>
+					<IconPickerControl
+						icon={ icon }
+						onChange={ ( result ) => {
+							if ( result !== undefined ) {
+								setAttributes( {
+									icon: result,
+								} );
+							}
+						} }
+					/>
+					<TextControl
+						label={ __( 'Alternatieve tekst' ) }
+						value={ iconAltText }
+						onChange={ ( value ) =>
+							setAttributes( {
+								iconAltText: value,
+							} )
+						}
+						help={ __(
+							'Voeg een alternatieve tekst toe als een icoon betekenis heeft.'
+						) }
+					/>
+				</PanelBody>
+			) }
 		</InspectorControls>
 	);
 };
