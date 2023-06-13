fef05947d355ee9974c8393d343777507b28297f
Lara van Diemen <lara@yard.nl>
2023-06-13 10:52:49 +0200

    (chore): refactor icon block

--- src/Blocks/icon/components/icon.js
+++
@@ -1,8 +0,0 @@
-const Icon = ( props ) => {
-	const { attributes } = props;
-	const { icon, altText } = attributes;
-
-	return <i className={ icon } title={ altText ? altText : null }></i>;
-};
-
-export default Icon;
