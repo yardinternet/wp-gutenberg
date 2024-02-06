e131d49e5e80eb07808646874dbbbc60d5db287c
Lara van Diemen <lara@yard.nl>
2024-02-06 10:19:12 +0100

    (feat): add counting-number block

---
+++ src/Blocks/counting-number/components/number.js
@@ -0,0 +1,24 @@
+const Number = ( props ) => {
+	const { attributes } = props;
+	const { number, numberPrefix, numberSuffix } = attributes;
+
+	return (
+		<>
+			{ numberPrefix && (
+				<span className="wp-block-yard-counting-number__prefix">
+					{ numberPrefix }
+				</span>
+			) }
+			<span className="wp-block-yard-counting-number__number">
+				{ number.replace( /\./g, ',' ) }
+			</span>
+			{ numberSuffix && (
+				<span className="wp-block-yard-counting-number__suffix">
+					{ numberSuffix }
+				</span>
+			) }
+		</>
+	);
+};
+
+export default Number;
