33491af43aae0a90fed3c2dae418da368bbd7038
Freek de Kruijff <freek.dekruijff@yard.nl>
2026-06-16 13:55:18 +0200

    chore: build

--- build/Blocks/table-of-contents/view.asset.php
+++ build/Blocks/table-of-contents/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element'), 'version' => '6b89b42100e2ca63a1d3');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element'), 'version' => '58ad5da55418aab4a660');
