d21ce4f111642102fde150664f58280dc64b7e53
YvetteNikolov <yvettenikolov@gmail.com>
2026-07-14 16:26:27 +0200

    chore: run npm run build

--- build/Blocks/table-of-contents/view.asset.php
+++ build/Blocks/table-of-contents/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element'), 'version' => '58ad5da55418aab4a660');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element'), 'version' => 'e292f5eb53615cb1dfcf');
